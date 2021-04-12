04070d1a572cf67ca543a6f1784437b531a196cf
izemaghilas <saidtala23@gmail.com>
2021-04-12 19:15:57 +0200

    remove column from table

---
+++ src/drawer/controller/removeColumnFromTable.ts
@@ -0,0 +1,49 @@
+import { dia } from "jointjs";
+
+import TypesEnumeration from "../view/ShapesTypes"
+
+function redefineEmbedsPosition(embdedCells: dia.Cell[], columnY: number): void{
+ 
+    /**
+     * ommit Label as it is in top
+     * ommit AddColumn => redefine its position after redefinig columns position
+     */
+    let addColumnCell = embdedCells.find(
+        cell=>cell.get("type") === TypesEnumeration.ADD_COLUMN_TYPE
+    ) as dia.Element
+    
+    embdedCells = embdedCells.filter(
+        cell => cell.get("type") !== TypesEnumeration.ADD_COLUMN_TYPE 
+                && 
+                cell.get("type") !== TypesEnumeration.LABEL_TYPE
+    )
+    
+    embdedCells.forEach(columnCell=>{
+        let columnCellY = columnCell.get("position").y
+        if(columnCellY > columnY){
+            columnCell.set("position", {x: columnCell.get("position").x, y: columnY})
+            columnCell.getEmbeddedCells().forEach(cell=>{
+                cell.set("position", {x: cell.get("position").x, y: columnY})
+            })
+            columnY = columnCellY
+        }
+    })
+    
+    addColumnCell.position(addColumnCell.get("position").x, columnY)
+}
+
+function removeColumnFromTable(removeColumnCell: dia.Cell) {
+    let ancestors = removeColumnCell.getAncestors()
+    
+    let column = ancestors[0] as dia.Element
+    const columnPosition = column.get("position")
+    column.remove()
+
+    let table = ancestors[ancestors.length-1] as dia.Element
+
+    redefineEmbedsPosition(table.getEmbeddedCells(), columnPosition.y)
+    table.fitEmbeds()
+
+}
+
+export default removeColumnFromTable
