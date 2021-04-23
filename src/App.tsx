9d95536d6b787a3f138ee3dfc60ad77b310f1c9f
izemaghilas <saidtala23@gmail.com>
2021-04-23 12:56:51 +0200

    delete useless comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-//import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-
 import 'jointjs/dist/joint.css'
 
 import Drawer from './drawer'
 
 function App() {
   return (
-    <div className="App">
-      <Drawer />
-    </div>
+    <Drawer/>
   );
 }
 
 export default App;
-
-/**
- * <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
- * 
- */
