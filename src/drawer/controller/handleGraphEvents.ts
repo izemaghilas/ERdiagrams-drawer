ad63f09b8932a416b95fed781b241d5928aba129
izemaghilas <saidtala23@gmail.com>
2021-04-27 14:17:19 +0200

    well events handling

--- src/drawer/controller/handleGraphEvents.ts
+++ src/drawer/controller/handleGraphEvents.ts
@@ -1,54 +1,66 @@
 import { dia } from "jointjs"
-
 import DrawerShapesFactory from "../view/DrawerShapesFactory"
 import TypesEnumeration from "../view/ShapesTypes"
 
-const handleGraphEvents = (graph : dia.Graph): void=>{
-    
-    graph.on("change:embeds", function(element: dia.Element){
-        element.fitEmbeds()
-    })
-    
-    graph.on("change:LabelValue", function(inputElement: dia.Element, value, opt){
+import getElementsResizeHandler from "./strategyPattern/ElementsResizeHandler"
+import getLabelChangeEventHandler from "./strategyPattern/LabelValueChangeEventHandler"
+import resizeTableOnColumnRemoved from "./strategyPattern/resizeTableOnColumnRemoved"
+
+
+function getTextWidth(label: dia.Element, paper: dia.Paper): number {
+    let labelView = label.findView(paper)
+    let LabelNode = labelView.findBySelector("label")[0]
+    let width = LabelNode.getBoundingClientRect().width
+    return width
+}
+
+const handleGraphEvents = (graph : dia.Graph, paper: dia.Paper): void=>{
+
+    graph.on("change:LabelValue", function(inputElement: dia.Element, value: string){
+
+        let position = inputElement.get("position") as {x: number; y: number}
+        let height = inputElement.size().height
+        let parent = inputElement.getParentCell() as dia.Element
+        inputElement.remove()
+        
         let label = DrawerShapesFactory.LABEL.getShape() as dia.Element
-        label.set("position", inputElement.get("position"))
-        label.resize(opt.length, inputElement.size().height)
+        label.set("position", position)
         label.attr("label/text", value)
         label.addTo(graph)
-
-        let parent = inputElement.getParentCell() as dia.Element
-        const parentSize = parent.size()
-        if(label.size().width < parentSize.width){
-            label.resize(parentSize.width-25, label.size().height)
-        }
-    
         parent.embed(label)
-        inputElement.remove()
-
-        if(parent.get("type") === TypesEnumeration.COLUMN_TYPE){
-            parent = parent.getParentCell() as dia.Element
-            parent.fitEmbeds()
-        }
-    
-        parent.getEmbeddedCells().forEach(cell=>{
-            let cellElement = cell as dia.Element
-            let cellSize: {width: number; height: number} = cell.get("size")
-            
-            if(cellSize.width < parent.get("size").width){
-                cellElement.resize(parent.get("size").width, cellSize.height)
-            }
-            
-            if(cellElement.get("type") === TypesEnumeration.COLUMN_TYPE){
-                cellElement.getEmbeddedCells().forEach(columnEmbdedCell=>{
-                    let columnEmbdedElement = columnEmbdedCell as dia.Element
-
-                    if(columnEmbdedCell.get("type") === TypesEnumeration.LABEL_TYPE){
-                        columnEmbdedElement.resize(cellElement.size().width-25, cellElement.size().height)
-                    }
-                })
-            }
-        })
         
+        let width = getTextWidth(label, paper)
+        // +10 to center text
+        label.size(width+10, height)
+
+        let handler = getLabelChangeEventHandler(parent.get("type"))
+        handler.handleEvent(parent, width)
+    })
+
+    graph.on("change:ColumnWidth", function(column: dia.Element, width: number){
+        let parent = column.getParentCell() as dia.Element
+        let handler = getElementsResizeHandler(parent.get("type"))
+        handler.resizeElement(parent, width)
+    })
+
+    graph.on("change:RemoveColumn", function(table: dia.Element, column: dia.Element){
+        
+        let label = column.getEmbeddedCells().find(
+            cell=>cell.get("type") === TypesEnumeration.LABEL_TYPE
+        ) as dia.Element
+        // +50 => remove column and pk width 
+        let width = getTextWidth(label, paper) + 50 
+
+        let widthState = table.get("widthState") as number[]
+        console.log(widthState);
+        
+        if(width === widthState[widthState.length-1]){
+            widthState.pop()
+            resizeTableOnColumnRemoved(table, widthState[widthState.length-1])
+        }
+
+        column.remove()
+
     })
     
 }
