a4d4ff6e6c673ba615d3d1213528a8ff8091b8b9
izemaghilas <saidtala23@gmail.com>
2021-08-27 15:13:41 +0200

    add instructions to user guide

--- src/drawer/components/userGuide/UsageCard.tsx
+++ src/drawer/components/userGuide/UsageCard.tsx
@@ -11,13 +11,20 @@ import UsageCardBody from './UsageCardBody'
 import './styles/usageCard.css'
 import { englishGuide, frenchGuide, GuideType, languages } from './guides'
 
-export default class UsageCard extends React.Component<{closeUserGuide():void},{userGuide: GuideType}> {
+export default class UsageCard extends React.Component<{closeUserGuide():void},{userGuide: GuideType, emptyInstructions: boolean}> {
 
     state = {
         userGuide: englishGuide,
+        emptyInstructions: false // used to remove current instructions on switching language
     }
 
-    setUserGuideLanguage(language: string){        
+    setEmptyInstructions(value: boolean){
+        this.setState({emptyInstructions: value})
+    }
+
+    setUserGuideLanguage(language: string){
+        this.state.userGuide.language !== language? this.setState({emptyInstructions: true}) : this.setState({emptyInstructions: false})
+        
         if(language === languages.french){
             this.setState({userGuide: frenchGuide})
         }
@@ -33,7 +40,11 @@ export default class UsageCard extends React.Component<{closeUserGuide():void},{
                     closeUsageCard={this.props.closeUserGuide} 
                     setUserGuideLanguage={this.setUserGuideLanguage.bind(this)}
                 />
-                <UsageCardBody userGuide={this.state.userGuide}/>
+                <UsageCardBody 
+                    userGuide={this.state.userGuide} 
+                    emptyInstructions={this.state.emptyInstructions}
+                    setEmptyInstructions={this.setEmptyInstructions.bind(this)}
+                />
             </div>
         )
     }
--- src/drawer/components/userGuide/UsageCardBody.tsx
+++ src/drawer/components/userGuide/UsageCardBody.tsx
@@ -1,24 +1,67 @@
-import React, { ReactElement } from "react"
+import React, { ReactElement, useState } from "react"
 import { GuideType } from "./guides"
 
 import './styles/usageCardBody.css'
 
 
-const ActionLabel: React.FC<{label: string}> = ({label}): ReactElement=>{
+const ActionLabel: React.FC<{
+    label: string, 
+    instructions: string[], 
+    setInstructions(instructions: string[]): void,
+    setEmptyInstructions(value: boolean): void
+}> 
+= ({label, instructions, setInstructions, setEmptyInstructions}): ReactElement=>{
     return (
-        <div className="usage-card-body-action-label-container">{label}</div>
+        <div 
+            className="usage-card-body-action-label-container"
+            onClick={()=>{
+                setInstructions(instructions)
+                setEmptyInstructions(false)
+            }}
+        >
+            {label}
+        </div>
     )
 }
 
-const UsageCardBody: React.FC<{userGuide: GuideType}> = ({userGuide}): ReactElement=>{
+const ActionInstruction: React.FC<{instruction: string}> = ({instruction}): ReactElement=>{
+    return (
+        <li className="usage-card-body-action-instruction-container">{instruction}</li>
+    )
+}
+
+const UsageCardBody: React.FC<{
+    userGuide: GuideType, 
+    emptyInstructions: boolean, 
+    setEmptyInstructions(value: boolean):void,
+}> 
+= ({userGuide, emptyInstructions, setEmptyInstructions}): ReactElement=>{
+    const [instructions, setInstructions] = useState<string[]>([])
+    
+    function showInstructions(instructions: string[]){
+        setInstructions(instructions)
+    }
+
     return (
         <div className="usage-card-body">
             <div className="usage-card-body-action-labels-container">
                 {
-                    userGuide.actions.map(action=>(<ActionLabel label={action.label} key={action.label}/>))
+                    userGuide.actions.map(action=>(
+                        <ActionLabel 
+                            label={action.label} 
+                            instructions={action.instructions}
+                            setInstructions={showInstructions}
+                            setEmptyInstructions={setEmptyInstructions}
+                            key={action.label}
+                        />
+                    ))
                 }
             </div>
-            <div className="usage-card-body-actions-container"></div>
+            <div className="usage-card-body-action-instructions-container">
+                {
+                    !emptyInstructions && instructions.map(instruction=>(<ActionInstruction instruction={instruction} key={instruction} />))
+                }
+            </div>
         </div>
     )
 }
